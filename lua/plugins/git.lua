cfa8e2559354f86a143ee5937e3db31d03cf094c
Braeden Hall <braeden@brhall.dev>
2026-01-30 12:03:24 -0800

    add diffview bindings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,5 +4,11 @@ require('gitsigns').setup {
 }
 
 -- better diff view for commits
+vim.keymap.set('n', '<leader>do', '<cmd>DiffviewOpen<CR>', { desc = 'Open diff view' })
+vim.keymap.set('n', '<leader>dc', '<cmd>DiffviewClose<CR>', { desc = 'Close diff view' })
+
+vim.keymap.set('n', '<leader>dl', '<cmd>diffget LOCAL<CR>', { desc = 'Take local changes' })
+vim.keymap.set('n', '<leader>dr', '<cmd>diffget REMOTE<CR>', { desc = 'Take remote changes' })
+
 vim.keymap.set('n', '<leader>db', '<cmd>DiffviewFileHistory<CR>', { desc = 'View diff for changes on current brach' })
 vim.keymap.set('n', '<leader>df', '<cmd>DiffviewFileHistory %<CR>', { desc = 'View diff for changes in current file' })
